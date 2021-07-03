e30c710ed122f5f4c697ea07c0ce2e631ffbab30
Jairo Juarez <jairojuarez20@hotmail.es>
2021-07-03 00:45:03 -0600

    Ejercicio5_JairoJuarez
    Entrega de ejercicio5, sección 1201, Jairo Juárez

---
+++ Ejercicio5_JairoJuarez/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace Ejercicio5_JairoJuarez
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtN = new System.Windows.Forms.TextBox();
+            this.TxtM = new System.Windows.Forms.TextBox();
+            this.BtnEjecutar = new System.Windows.Forms.Button();
+            this.BtnLimpiar = new System.Windows.Forms.Button();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 171);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese cantidad de N (Filas)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 239);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(252, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese cantidad de M (Columnas)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(73, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(482, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ingrese el tamaño que desea tener en una matriz";
+            // 
+            // TxtN
+            // 
+            this.TxtN.Location = new System.Drawing.Point(378, 168);
+            this.TxtN.Name = "TxtN";
+            this.TxtN.Size = new System.Drawing.Size(158, 26);
+            this.TxtN.TabIndex = 3;
+            // 
+            // TxtM
+            // 
+            this.TxtM.Location = new System.Drawing.Point(378, 233);
+            this.TxtM.Name = "TxtM";
+            this.TxtM.Size = new System.Drawing.Size(158, 26);
+            this.TxtM.TabIndex = 4;
+            // 
+            // BtnEjecutar
+            // 
+            this.BtnEjecutar.Location = new System.Drawing.Point(78, 342);
+            this.BtnEjecutar.Name = "BtnEjecutar";
+            this.BtnEjecutar.Size = new System.Drawing.Size(107, 57);
+            this.BtnEjecutar.TabIndex = 5;
+            this.BtnEjecutar.Text = "Ejecutar";
+            this.BtnEjecutar.UseVisualStyleBackColor = true;
+            this.BtnEjecutar.Click += new System.EventHandler(this.BtnEjecutar_Click);
+            // 
+            // BtnLimpiar
+            // 
+            this.BtnLimpiar.Location = new System.Drawing.Point(272, 342);
+            this.BtnLimpiar.Name = "BtnLimpiar";
+            this.BtnLimpiar.Size = new System.Drawing.Size(96, 57);
+            this.BtnLimpiar.TabIndex = 6;
+            this.BtnLimpiar.Text = "Limpiar";
+            this.BtnLimpiar.UseVisualStyleBackColor = true;
+            this.BtnLimpiar.Click += new System.EventHandler(this.BtnLimpiar_Click);
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.Location = new System.Drawing.Point(459, 338);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(96, 61);
+            this.BtnExit.TabIndex = 7;
+            this.BtnExit.Text = "Exit";
+            this.BtnExit.UseVisualStyleBackColor = true;
+            this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 420);
+            this.Controls.Add(this.BtnExit);
+            this.Controls.Add(this.BtnLimpiar);
+            this.Controls.Add(this.BtnEjecutar);
+            this.Controls.Add(this.TxtM);
+            this.Controls.Add(this.TxtN);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtN;
+        private System.Windows.Forms.TextBox TxtM;
+        private System.Windows.Forms.Button BtnEjecutar;
+        private System.Windows.Forms.Button BtnLimpiar;
+        private System.Windows.Forms.Button BtnExit;
+    }
+}
+
---
+++ Ejercicio5_JairoJuarez/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio5_JairoJuarez
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BtnLimpiar_Click(object sender, EventArgs e)
+        {
+            TxtM.Clear();
+            TxtN.Clear();
+        }
+
+        private void BtnEjecutar_Click(object sender, EventArgs e)
+        {
+            int Fila; Fila = Convert.ToInt32(TxtN.Text);
+            int Columna; Columna = Convert.ToInt32(TxtM.Text);
+
+            Random Aleatorios = new Random();
+
+            int[,] Matriz = new int[Fila, Columna];
+
+            for (int N = 0; N < Matriz.GetLength(1); N++)
+            {
+                for (int M = 0; M < Matriz.GetLength(1); N++)
+                {
+                    Matriz[N, M] = (Aleatorios.Next(1, 99));
+                    MessageBox.Show(N + " , " + M + " = " + Matriz[N, M]);
+
+                }
+            }
+        }
+    }
+
+}
